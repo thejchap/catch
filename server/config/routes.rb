5a49bf49b7fe88cec9396ce23efee6be80908b6c
Justin Chapman <commonmodestudio@gmail.com>
2018-12-05 01:13:35 -0500

    route glob

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post '/api/graphql', to: "api/graphql#execute"
-  root 'application#index'
-
   use_doorkeeper
+
+  get 'me*', to: 'application#index'
+  root 'application#index'
 end
