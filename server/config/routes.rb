463349a1a4561967a0aaf385eecd64465601a085
Justin Chapman <commonmodestudio@gmail.com>
2018-12-05 01:20:36 -0500

    fix path

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   post '/api/graphql', to: "api/graphql#execute"
   use_doorkeeper
 
-  get '/me*path', to: 'application#index'
+  get '/me', to: 'application#index'
+  get '/me/*path', to: 'application#index'
   root 'application#index'
 end
