a6c82ec2a31d3f50284796e01a3c74d5846ce4d1
Justin Chapman <commonmodestudio@gmail.com>
2018-12-12 16:00:56 -0500

    Fix

--- server/config/initializers/sidekiq.rb
+++ server/config/initializers/sidekiq.rb
@@ -1,8 +1,3 @@
 Sidekiq.configure_server do |config|
   config.average_scheduled_poll_interval = 2
-  config.redis = Catch::Redis.redis
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = Catch::Redis.redis
 end
