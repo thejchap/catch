bde1cbfb9440746544ad588bf8c5b515c67339ed
Justin Chapman <commonmodestudio@gmail.com>
2019-02-25 12:00:37 -0700

    Update deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+REDIS_URL=$(cd server && DISABLE_SPRING=1 bin/rails runner "puts Catch::Application.credentials.dig :production, :redis_url")
 (cd client/web && ember deploy production)
 git subtree push --prefix server heroku master
